00bcf77d3b2e5fd7356c6ace9e4b956539ee54b0
David Brown <dmlb2000@gmail.com>
2017-05-24 07:17:09 -0700

    we just need to not run the core phpunit tests

--- travis/script.sh
+++ travis/script.sh
@@ -14,11 +14,7 @@ else
   ./../vendor/bin/drush site-install --verbose --yes --db-url=sqlite://tmp/site.sqlite
   ./../vendor/bin/drush runserver $SIMPLETEST_BASE_URL &
   until curl -s $SIMPLETEST_BASE_URL; do true; done > /dev/null
-  # Skip core/tests/Drupal/Tests/ComposerIntegrationTest.php because web/ has no composer.json
-  # Ignore PageCache group temporarily, @see https://www.drupal.org/node/2770673
-  ./../vendor/bin/phpunit -c core --testsuite unit --exclude-group Composer,DependencyInjection,PageCache
-  ./../vendor/bin/drush
-  if [[ $RELEASE = stable ]]; then
-    ./../vendor/bin/drupal
-  fi
+  # We'll need to uncomment these when we get some real tests.
+  # ./../vendor/bin/phpunit
+  # ./../vendor/bin/drush
 fi
